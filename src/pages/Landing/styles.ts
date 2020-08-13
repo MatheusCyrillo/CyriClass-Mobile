318cefa2fda3cd64d89dc1eaee6148eee15dfd46
MatheusCyrillo <marc.cyrillo@gmail.com>
2020-08-12 21:34:58 -0300

    estruturacao e estilizacao da landing prontas

--- src/pages/Landing/styles.ts
+++ src/pages/Landing/styles.ts
@@ -14,6 +14,7 @@ banner: {
 },
 
 title: {
+    fontFamily: 'Poppins_400Regular',
     color: '#fff',
     fontSize: 20,
     lineHeight: 30,
@@ -21,8 +22,48 @@ title: {
 },
 
 titleBold:{
-    fontWeight: 'bold'
+    fontFamily: 'Poppins_600SemiBold'
+},
+
+buttonsContainer: {
+    flexDirection: 'row',
+    marginTop: 40,
+    justifyContent: 'space-between'
+},
+
+button:{
+    height: 150,
+    width: '48%',
+    backgroundColor: '#333',
+    borderRadius: 8,
+    padding: 24,
+    justifyContent: 'space-between'
+},
+
+buttonPrimary: {
+    backgroundColor: '#9871f5',
+},
+
+buttonSecondary: {
+    backgroundColor: '#04d361'
+},
+
+buttonText: {
+    fontFamily: 'Archivo_700Bold',
+    color: '#FFF',
+    fontSize: 20,
+
+},
+
+totalConnections: {
+    fontFamily: 'Poppins_400Regular',
+    color: '#d4c2ff',
+    fontSize: 12,
+    lineHeight: 20,
+    maxWidth: 160,
+    marginTop: 40
 }
+
 });
 
 export default styles;
